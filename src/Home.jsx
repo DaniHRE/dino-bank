8812daab0f251dd6492f08d0bf0314499ae4fe7e
Daniel Henrique <henriqueevaldo@outlook.com>
2022-08-17 11:17:05 -0300

    change dependencies

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import './Home.css'
-import appBar from './components/appBar/appBar'
+import { ResponsiveAppBar } from './components/ResponsiveAppBar/ResponsiveAppBar'
 
 function Home() {
   return (
     <div className="Home">
-      <div className='left'>
-
-      </div>
-      <div className='right'>
-
-      </div>
+      <ResponsiveAppBar />
     </div>
   )
 }
---
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.jsx
@@ -0,0 +1,5 @@
+export const ResponsiveAppBar = () => {
+    return (
+
+    );
+}
