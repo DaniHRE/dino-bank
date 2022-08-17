ad24783ecf48434fae579f480d1535ced399930c
Daniel Henrique Rodrigues Evaldo da Silva <52361068850>
2022-08-17 09:38:47 -0300

    initializing repo

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './Home.css'
+import appBar from './components/appBar/appBar'
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className='left'>
+
+      </div>
+      <div className='right'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
