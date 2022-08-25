48f29011646c763d46b90c1fc9aeb655ad9483d9
Daniel Henrique <henriqueevaldo@outlook.com>
2022-08-25 09:06:42 -0300

    added Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Bundler } from './Bundler'
-// import { MantineProvider } from '@mantine/core';
+// REACT IMPORTS
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// STYLE IMPORTS
+import { MantineProvider } from '@mantine/core';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Bundler />
-  </React.StrictMode >
-)
+// PAGES
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { Register } from './pages/Register/Register';
+
+const rootElement = document.getElementById("root");
+render(
+  <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'light' }} >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </MantineProvider >,
+  rootElement
+);
