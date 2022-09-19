7da8dd943396fccac7d392288e4c8e244964f283
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-19 09:47:43 -0300

    changed Hero Template

--- src/components/Hero/HeroDots/index.tsx
+++ src/components/Hero/HeroDots/index.tsx
@@ -8,6 +8,7 @@ import {
     List,
     ThemeIcon,
 } from '@mantine/core';
+import { StatsRing } from '../../Stats/StatsProgress';
 import { IconCheck } from '@tabler/icons';
 import { useStyles } from './style';
 
@@ -17,19 +18,18 @@ export function HeroDots() {
         <div>
             <Container>
                 <h1 className={classes.title}>
-                    A{' '}
+                    Work With Your{' '}
                     <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
-                        fully featured
+                        Money.
                     </Text>{' '}
-                    React components and hooks library
                 </h1>
 
-                <Text className={classes.description} color="dimmed">
-                    Build fully functional accessible web applications with ease – Mantine includes more than
-                    100 customizable components and hooks to cover you in any situation
+                <Text className={classes.description} mb="2rem" >
+                    Dino Bank is the smartest way to plan, save and investment with
+                    impresive interest rates, and app, tools & guides.
                 </Text>
 
-                <List
+                {/* <List
                     mt={30}
                     spacing="sm"
                     size="sm"
@@ -51,7 +51,27 @@ export function HeroDots() {
                         <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
                         keyboard
                     </List.Item>
-                </List>
+                </List> */}
+
+                <StatsRing data={[{
+                    label: "New Users",
+                    stats: "77,250",
+                    progress: 90,
+                    color: "#3DB2F0",
+                    icon: 'up',
+                }, {
+                    label: "Transactions",
+                    stats: "849,750",
+                    progress: 40,
+                    color: "#2B6ED9",
+                    icon: 'up',
+                }, {
+                    label: "Acquired Shares",
+                    stats: "849,750",
+                    progress: 60,
+                    color: "#324AF8",
+                    icon: 'up',
+                }]} />
 
                 <Group className={classes.controls}>
                     <Button
