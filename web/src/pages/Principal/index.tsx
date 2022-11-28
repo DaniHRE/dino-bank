276057f5a6fcd2838430ab658d77a1fe69b2ee8f
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-28 11:32:29 -0300

    update: change fetch to api methods

--- web/src/pages/Principal/index.tsx
+++ web/src/pages/Principal/index.tsx
@@ -1,55 +1,46 @@
 import { useEffect, useState } from "react";
+import { Auth } from "../../utils/api";
 import { HeaderLogged } from "../../components/Headers/HeaderLogged";
 import { useStyles } from "./style";
 
 export function Principal() {
     const { classes } = useStyles();
     const [name, setName] = useState('');
+    const [token, setToken] = useState('');
+    let userData = {};
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    token! ? Auth.user(config).then(data => {userData = data, console.log(userData)}) : null
+
+    const getUser = async () => {
+        Auth.refresh().then(data => setToken(data.token))
+    }
 
     useEffect(() => {
-        (
-            async () => {
                 // const token = await fetch('http://localhost:8000/api/logout', {
                 //     method: 'POST',
                 //     headers: {"Content-type": "application/json; charset=UTF-8"}
                 // })
 
-                const cavalo = await fetch('http://localhost:8000/api/login', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    credentials: 'include',
-                    body: JSON.stringify({
-                        email: 'a@a.com',
-                        password: 'a'
-                    })
-                });
+                // const cavalo = await fetch('http://localhost:8000/api/login', {
+                //     method: 'POST',
+                //     headers: { 'Content-Type': 'application/json' },
+                //     credentials: 'include',
+                //     body: JSON.stringify({
+                //         email: 'a@a.com',
+                //         password: 'a'
+                //     })
+                // });
 
-                console.log(cavalo)
+                // console.log(cavalo)
 
-                const token = await fetch('http://localhost:8000/api/refresh', {
-                    method: 'POST',
-                    headers: { "Content-type": "application/json" },
-                    credentials: 'include'
-                })
-                    .then(response => response.json())
-
-                console.log(token)
-
-                const response = await fetch('http://localhost:8000/api/user', {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token.token}`
-                    },
-                    credentials: 'include',
-                });
-
-                const content = await response.json();
-                console.log(content);
-
-                setName(content.first_name);
-            }
-        )();
-    });
+                getUser()
+    }, []);
 
     return (
         <HeaderLogged user={{ name: name, image: "" }} tabs={[]} />
