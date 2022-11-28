eb743a2189a361aebb9c53cb864a95ba02d64420
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-28 11:30:02 -0300

    feat(api): api methods

---
+++ web/src/utils/api/index.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
+import { IAuth, IAuthPost } from '../../models/Auth';
+
+const baseURL = 'http://localhost:8000/api/'
+
+const instance = axios.create({
+    baseURL: baseURL,
+    timeout: 15000,
+});
+
+const postInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 15000,
+    withCredentials: true,
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string, config?: {}) => instance.get(url, config).then(responseBody),
+    post: (url: string, body?: {}, config?: {}) => postInstance.post(url, body, config).then(responseBody),
+    put: (url: string, body: {}) => postInstance.put(url, body).then(responseBody),
+    delete: (url: string) => instance.delete(url).then(response => response),
+};
+
+export const Auth = {
+    login: (credentials: IAuth): Promise<IAuth> => requests.post('login', credentials),
+    register: (credentials: IAuthPost): Promise<IAuthPost> => requests.post('register', credentials),
+    refresh: (): Promise<any> => requests.post('refresh'),
+    user: (config: {}): Promise<any> => requests.get('user', config)
+};
