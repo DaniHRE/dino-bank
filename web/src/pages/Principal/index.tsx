86091dc83f45b7941584c61341c87543f90ee515
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-29 09:02:53 -0300

    feat: login feature

--- web/src/pages/Principal/index.tsx
+++ web/src/pages/Principal/index.tsx
@@ -2,47 +2,42 @@ import { useEffect, useState } from "react";
 import { Auth } from "../../utils/api";
 import { HeaderLogged } from "../../components/Headers/HeaderLogged";
 import { useStyles } from "./style";
+import { IAuth } from "src/models/Auth";
+import { useNavigate } from "react-router-dom";
 
 export function Principal() {
     const { classes } = useStyles();
-    const [name, setName] = useState('');
-    const [token, setToken] = useState('');
-    let userData = {};
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState<IAuth>();
 
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-
-    token! ? Auth.user(config).then(data => {userData = data, console.log(userData)}) : null
-
-    const getUser = async () => {
-        Auth.refresh().then(data => setToken(data.token))
+    const getUser = () => {
+        Auth.user()
+            .then(data => setUserData(data))
+            .catch(error => navigate('/'))
     }
 
     useEffect(() => {
-                // const token = await fetch('http://localhost:8000/api/logout', {
-                //     method: 'POST',
-                //     headers: {"Content-type": "application/json; charset=UTF-8"}
-                // })
+        // const token = await fetch('http://localhost:8000/api/logout', {
+        //     method: 'POST',
+        //     headers: {"Content-type": "application/json; charset=UTF-8"}
+        // })
 
-                // const cavalo = await fetch('http://localhost:8000/api/login', {
-                //     method: 'POST',
-                //     headers: { 'Content-Type': 'application/json' },
-                //     credentials: 'include',
-                //     body: JSON.stringify({
-                //         email: 'a@a.com',
-                //         password: 'a'
-                //     })
-                // });
+        // const cavalo = await fetch('http://localhost:8000/api/login', {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     credentials: 'include',
+        //     body: JSON.stringify({
+        //         email: 'a@a.com',
+        //         password: 'a'
+        //     })
+        // });
 
-                // console.log(cavalo)
+        // console.log(cavalo)
 
-                getUser()
+        getUser()
     }, []);
 
     return (
-        <HeaderLogged user={{ name: name, image: "" }} tabs={[]} />
+        <HeaderLogged user={{ name: userData?.first_name, image: "" }} tabs={[]} />
     )
 }
