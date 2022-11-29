0d48ae88867e238a4790c4c9630e6cce50ec75bf
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-29 09:02:20 -0300

    fix: auth interface return types]

--- web/src/models/Auth/index.ts
+++ web/src/models/Auth/index.ts
@@ -1,7 +1,12 @@
 export interface IAuth {
-
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    bankaccount: any[];
+    transactions: any[];
 }
 
 export interface IAuthPost {
-    
+
 }
