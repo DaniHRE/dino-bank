fff915f5f39b0dd7c761584472f7f50c59aa7a56
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-29 23:41:16 -0300

    update: home screen register button

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
-import { Paper } from "@mantine/core";
+import { Button, Group, Paper, Stack } from "@mantine/core";
 import { HeaderDefault } from "../../components/Headers/HeaderDefault";
 import { HeroDots } from "../../components/Hero/HeroDots";
 import { Card } from "react-pay-card";
 import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage';
 import { useStyles } from './style';
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
   const { classes } = useStyles();
+  const navigate = useNavigate();
 
   return (
     <div className="App">
@@ -20,7 +22,7 @@ export function Home() {
             </FullpageSection>
 
             <FullpageSection>
-              <div className={ classes.card }>
+              <Stack align="center"  justify="center" className={classes.card}>
                 <Card
                   cardCvv='000'
                   cardHolder='JOHN DOE'
@@ -28,7 +30,17 @@ export function Home() {
                   cardYear='2026'
                   cardNumber='0000 0000 0000 0000'
                 />
-              </div>
+                <Button
+                  mt={50}
+                  radius={10}
+                  variant="light"
+                  size="xl"
+                  gradient={{ from: 'blue', to: 'cyan' }}
+                  onClick={() => navigate('/register')}
+                >
+                  Create your free account
+                </Button>
+              </Stack>
             </FullpageSection>
 
           </FullPageSections>
