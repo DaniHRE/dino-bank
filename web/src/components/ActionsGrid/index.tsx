310ce3ec8fd4afded92e9c388b02aadadb0885dd
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-30 09:44:03 -0300

    feat: actions grid component

---
+++ web/src/components/ActionsGrid/index.tsx
@@ -0,0 +1,52 @@
+import {
+  IconCreditCard,
+  IconBuildingBank,
+  IconRepeat,
+  IconReceiptRefund,
+  IconReceipt,
+  IconReceiptTax,
+  IconReport,
+  IconCashBanknote,
+  IconCoin,
+} from '@tabler/icons';
+import { Card, Text, SimpleGrid, UnstyledButton, Anchor, Group } from '@mantine/core';
+import { useStyles } from './style';
+
+const mockdata = [
+  { title: 'Credit cards', icon: IconCreditCard, color: 'violet' },
+  { title: 'Banks nearby', icon: IconBuildingBank, color: 'indigo' },
+  { title: 'Transfers', icon: IconRepeat, color: 'blue' },
+  { title: 'Refunds', icon: IconReceiptRefund, color: 'green' },
+  { title: 'Receipts', icon: IconReceipt, color: 'teal' },
+  { title: 'Taxes', icon: IconReceiptTax, color: 'cyan' },
+  { title: 'Reports', icon: IconReport, color: 'pink' },
+  { title: 'Payments', icon: IconCoin, color: 'red' },
+  { title: 'Cashback', icon: IconCashBanknote, color: 'orange' },
+];
+
+export function ActionsGrid() {
+  const { classes, theme } = useStyles();
+
+  const items = mockdata.map((item) => (
+    <UnstyledButton key={item.title} className={classes.item}>
+      <item.icon color={theme.colors[item.color][6]} size={32} />
+      <Text size="xs" mt={7}>
+        {item.title}
+      </Text>
+    </UnstyledButton>
+  ));
+
+  return (
+    <Card withBorder radius="md" className={classes.card}>
+      <Group position="apart">
+        <Text className={classes.title}>Services</Text>
+        <Anchor size="xs" color="dimmed" sx={{ lineHeight: 1 }}>
+          + Infinite other services
+        </Anchor>
+      </Group>
+      <SimpleGrid cols={3} mt="md">
+        {items}
+      </SimpleGrid>
+    </Card>
+  );
+}
---
+++ web/src/components/ActionsGrid/style.tsx
@@ -0,0 +1,29 @@
+import { createStyles } from "@mantine/core";
+
+export const useStyles = createStyles((theme) => ({
+    card: {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+
+    title: {
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        fontWeight: 700,
+    },
+
+    item: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        borderRadius: theme.radius.md,
+        height: 90,
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+        transition: 'box-shadow 150ms ease, transform 100ms ease',
+
+        '&:hover': {
+            boxShadow: `${theme.shadows.md} !important`,
+            transform: 'scale(1.05)',
+        },
+    },
+}));
