faaff55e477ac5e8015de6dbe71cad78cf9c33ef
Daniel Henrique <henriqueevaldo@outlook.com>
2022-12-01 00:51:08 -0300

    feat: custom AppShell component

---
+++ web/src/components/GridAppShell/index.tsx
@@ -0,0 +1,67 @@
+import {
+    AppShell,
+    Footer,
+    useMantineTheme,
+} from '@mantine/core';
+import { HeaderLogged } from "../Headers/HeaderLogged";
+import { Profile } from "../../pages/Profile";
+import { NavbarMinimal } from "../NavbarMinimal";
+import { FooterCentered } from '../Footer';
+import { useEffect, useState } from "react";
+import { Auth } from "../../utils/api";
+import { useStyles } from "./style";
+import { IAuth } from "../../models/Auth";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ActionsGrid } from '../ActionsGrid';
+import { Transfer } from '../../pages/Transfer';
+
+export function GridAppShell() {
+    const { classes } = useStyles();
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState<IAuth>();
+
+    const theme = useMantineTheme();
+    const [opened, setOpened] = useState(false);
+
+    const getUser = () => {
+        Auth.user()
+            .then(data => setUserData(data))
+            .catch(error => navigate('/'))
+    }
+
+    useEffect(() => {
+        getUser()
+    }, []);
+
+    return (
+        <div className={classes.root}>
+            <AppShell
+                styles={{
+                    main: {
+                        background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+                    },
+                }}
+                navbarOffsetBreakpoint="sm"
+                asideOffsetBreakpoint="sm"
+                navbar={
+                    <NavbarMinimal hidden={opened} />
+                }
+                footer={
+                    <Footer height={60}>
+                        <FooterCentered links={[]} />
+                    </Footer>
+                }
+                header={
+                    <HeaderLogged user={{ name: userData?.first_name, image: '' }} />
+                }
+            >
+                <Routes>
+                    <Route path="/" element={<ActionsGrid/>} />
+                    <Route path="transfer" element={<Transfer />} />
+                    <Route path="profile" element={<Profile data={userData} />} />
+                    <Route path="settings" element={<h1> BBBBBBBBBBBBB</h1>} />
+                </Routes>
+            </AppShell>
+        </div>
+    )
+}
